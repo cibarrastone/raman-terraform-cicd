6ed79f7c27bc82c731b162318889a3b403242f28
Christopher Stone <chriibar@cisco.com>
2025-12-19 09:04:42 -0600

    chore: updated naming references

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cisco-raman-terraform-state-bucket"
+    bucket         = "cisco-chris-terraform-state-bucket"
     key            = "ci/aws/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "cisco-raman-terraform-state-lock"
+    dynamodb_table = "cisco-chris-terraform-state-lock"
     encrypt        = true
   }
 }
